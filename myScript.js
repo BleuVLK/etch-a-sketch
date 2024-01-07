71e4ff32306412ce4f00d5c47e16d890a99e9b0c
Bleu <147571480+BleuVLK@users.noreply.github.com>
2024-01-07 19:54:49 -0300

    Add permanent black trail on hover

--- myScript.js
+++ myScript.js
@@ -7,18 +7,25 @@ let row = 16;
 let column = 16;
 
 function generateGrid(row, column) {
-  const BOX = document.querySelector('.grid');
+  /* generates a grid by creating an amount of rows which is 
+  filled by a given amount of items through a loop*/
+  const BOX = document.querySelector('.grid-box');
   for (let i = 0; i <= row; i++) {
     const GRID_ROW = document.createElement('div');
     GRID_ROW.classList.toggle('row');
-    GRID_ROW.style.cssText = 'border: 1px solid black; display: flex; gap: 1em;'
+    GRID_ROW.style.cssText = 'display: flex;'
     for (let c = 0; c <= column; c++) {
       const GRID_COLUMN = document.createElement('div');
       GRID_COLUMN.classList.toggle('column');
-      GRID_COLUMN.style.cssText = 'border: 1px solid black;'
-      GRID_COLUMN.textContent = c;
+      GRID_COLUMN.addEventListener('mouseover', () => {
+        changeColour(GRID_COLUMN);
+      })
       GRID_ROW.append(GRID_COLUMN);
     }
     BOX.append(GRID_ROW);
   }
+}
+
+function changeColour(element) {
+  element.classList.add('change-colour');
 }
